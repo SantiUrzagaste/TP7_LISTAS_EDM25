de9769605991c9e9b2ffbf028303ddf91761d134
SantiUrzagaste <urzagastesantiago95@gmail.com>
2025-06-26 12:41:36 -0300

    p4 archivo
    es el primer archivo cpp que se sube del p4

---
+++ punto4 - to .cpp
@@ -0,0 +1,99 @@
+
+#include <iostream>
+using namespace std;
+
+class Nodo {
+private:
+    int dato;
+    Nodo* sig;
+public:
+    Nodo(int dato) {
+        this->dato = dato;
+        this->sig = nullptr;
+    }
+    int getDato() { return dato; }
+    Nodo* getSiguiente() { return sig; }
+    void setSiguiente(Nodo* nuevo) { sig = nuevo; }
+};
+
+class Pila {
+private:
+    Nodo* tope;
+public:
+    Pila() {
+        tope = nullptr;
+    }
+
+    void apilar(Nodo* nuevo) {
+        nuevo->setSiguiente(tope);
+        tope = nuevo;
+    }
+
+    Nodo* desapilar() {
+        Nodo* borrado = nullptr;
+        if (tope != nullptr) {
+            borrado = tope;
+            tope = tope->getSiguiente();
+            borrado->setSiguiente(nullptr);
+        }
+        return borrado;
+    }
+
+    Nodo* verTope() {
+        return tope;
+    }
+};
+
+
+class Cola {
+private:
+    Nodo* frente;
+    Nodo* fin;
+    int cant;
+public:
+    Cola() {
+        frente = nullptr;
+        fin = nullptr;
+        cant = 0;
+    }
+
+    void encolar(Nodo* nuevo) {
+        if (frente == nullptr) {
+            frente = fin = nuevo;
+        } else {
+            fin->setSiguiente(nuevo);
+            fin = nuevo;
+        }
+        cant++;
+    }
+
+    Nodo* desencolar() {
+        Nodo* borrado = nullptr;
+        if (frente != nullptr) {
+            borrado = frente;
+            frente = frente->getSiguiente();
+            borrado->setSiguiente(nullptr);
+            if (frente == nullptr) fin = nullptr;
+            --cant;
+        }
+        return borrado;
+    }
+
+    int getCantidad() {
+        return cant;
+    }
+};
+
+int main() {
+    Pila pila;
+    pila.apilar(new Nodo(1));
+    pila.apilar(new Nodo(2));
+    cout << "Tope de la pila: " << pila.verTope()->setDato() ;
+
+    Cola cola;
+    cola.encolar(new Nodo(5));
+    cola.encolar(new Nodo(6));
+    cout << "Cantidad en cola: " << cola.getCantidad() ;
+
+    return 0;
+}
