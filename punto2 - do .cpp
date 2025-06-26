a17f25807e3d7ae4101079875f1d476caea0f0d9
SantiUrzagaste <urzagastesantiago95@gmail.com>
2025-06-26 12:21:46 -0300

    p2 archivo
    primer archivo de p2

---
+++ punto2 - do .cpp
@@ -0,0 +1,68 @@
+
+#include <iostream>
+using namespace std;
+
+class Nodo {
+private:
+    int dato;
+    Nodo* sig;
+public:
+    Nodo(int dato) {
+        this->dato = dato;
+        this->sig = nullptr;
+    }
+    int getDato() { return dato; }
+    Nodo* getSiguiente() { return sig; }
+    void setSiguiente(Nodo* nuevo) { sig = nuevo; }
+};
+
+class Lista {
+private:
+    Nodo* inicio;
+    Nodo* fin;
+    int cant;
+public:
+    Lista() {
+        inicio = nullptr;
+        fin = nullptr;
+        cant = 0;
+    }
+
+    void AgregarFinal(Nodo* nuevo) {
+        if (inicio == nullptr) {
+            inicio = fin = nuevo;
+        } else {
+            fin->setSiguiente(nuevo);
+            fin = nuevo;
+        }
+        cant++;
+    }
+
+    Nodo* EliminarInicio() {
+        Nodo* borrado = nullptr;
+        if (inicio != nullptr) {
+            borrado = inicio;
+            inicio = inicio->getSiguiente();
+            if (inicio == nullptr) fin = nullptr;
+            cant--;
+        }
+        return borrado;
+    }
+
+    int getCantidad() {
+        return cant;
+    }
+}
+
+int main() {
+    Lista lista;
+    lista.AgregarFinal(new Nodo(1));
+    lista.AgregarFinal(new Nodo(2));
+    lista.AgregarFinal(new Nodo(3));
+
+    cout << "Cantidad: " << getCantidad() << endl;
+    EliminarInicio();
+    cout << "Cantidad tras eliminar: " << getCantidad() << endl;
+
+    return 0;
+}
